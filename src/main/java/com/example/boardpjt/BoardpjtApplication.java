252b64ed85d3c7e906f955b0494bdf3b2211f213
notlikelion <notlikelion@gmail.com>
2025-09-08 13:42:12 +0900

    푸시

--- src/main/java/com/example/boardpjt/BoardpjtApplication.java
+++ src/main/java/com/example/boardpjt/BoardpjtApplication.java
@@ -1,7 +1,11 @@
 package com.example.boardpjt;
 
+import com.example.boardpjt.model.entity.UserAccount;
+import com.example.boardpjt.model.repository.UserAccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BoardpjtApplication {
@@ -10,4 +14,20 @@ public class BoardpjtApplication {
 		SpringApplication.run(BoardpjtApplication.class, args);
 	}
 
+    @Bean
+    public CommandLineRunner initAdmin(UserAccountRepository userAccountRepository) {
+        return args -> {
+                String adminUsername = "admin";
+                String adminPassword = "pass";
+                boolean adminExists = userAccountRepository.findByUsername(adminUsername).isPresent();
+
+                if (!adminExists) { // 기존에 추가된 어드민이 없으면...
+                    UserAccount admin = new UserAccount();
+                    admin.setUsername(adminUsername);
+                    admin.setPassword(adminPassword);
+                    admin.setRole("ROLE_ADMIN");
+                    userAccountRepository.save(admin);
+                }
+        };
+    }
 }
