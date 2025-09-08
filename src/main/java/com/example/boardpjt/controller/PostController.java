8cb4e82b0fbf2babfa44848b6814e1dd50d51a3d
notlikelion <notlikelion@gmail.com>
2025-09-08 15:30:08 +0900

    푸시

--- src/main/java/com/example/boardpjt/controller/PostController.java
+++ src/main/java/com/example/boardpjt/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.example.boardpjt.controller;
 
+import com.example.boardpjt.model.dto.PostDTO;
 import com.example.boardpjt.service.PostService;
 import com.example.boardpjt.service.UserAccountService;
 import lombok.RequiredArgsConstructor;
@@ -17,8 +18,28 @@ public class PostController {
     private final PostService postService;
 
     // 게시물 목록
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("posts", postService.findAll());
+        return "post/list"; // templates/post/list.html
+    }
     // 개별 게시물
+    @GetMapping("/{id}")
+    public String list(
+            @PathVariable Long id,
+            Model model) {
+        // 각각 개별이니까... 1개.
+        model.addAttribute("post", postService.findById(id));
+        return "post/detail"; // templates/post/list.html
+    }
     // 게시물 작성
+    @GetMapping("/new")
+    public String createForm(
+            Model model) {
+        model.addAttribute("post", new PostDTO.Request());
+        return "post/form"; // templates/post/list.html
+    }
+    // POST 처리...
 
     // 수정
     // 삭제
